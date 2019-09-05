e01de417f5e5c2433f8a4a495901651f7183e675
Armin Abbasi <armin.abbasi@hotmail.com>
2019-09-05 15:37:11 +0430

    added necessary routes and controllers

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\CredentialRequest;
 use App\Libraries\Api\Response;
 use App\Models\User;
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,26 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'v1'], function () {
-    // Authentication routes
-    Route::post('/users/login', 'AuthController@login');
-    Route::get('/users/logout', 'AuthController@logout');
-    Route::post('/users/register', 'AuthController@register');
+
+    Route::group(['prefix' => 'users'], function () {
+        // Authentication routes
+        Route::post('/login', 'AuthController@login');
+        Route::get('/logout', 'AuthController@logout');
+        Route::post('/register', 'AuthController@register');
+    });
+
+    Route::group(['prefix' => 'admins', 'namespace' => 'Admin', 'middleware' => 'isAdmin'], function () {
+        // User management routes
+        Route::post('/users', 'UserController@createUser');
+        Route::get('/users', 'UserController@listUsers');
+        Route::delete('/users/{user}', 'UserController@deleteUser');
+
+        // Group management routes
+        Route::post('/groups', 'GroupController@createGroup');
+        Route::get('/groups', 'GroupController@listGroups');
+        Route::get('/groups/{group}/user/{user}', 'GroupController@attachUser');
+        Route::delete('/groups/{group}/user/{user}', 'GroupController@detachUser');
+        Route::delete('/groups/{group}', 'GroupController@deleteGroup');
+    });
+
 });
